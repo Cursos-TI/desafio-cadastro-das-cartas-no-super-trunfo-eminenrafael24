848df6f1454d945521ab87f2b00149bb20bcb94e
eminenrafael24 <macaewebnews@gmail.com>
2025-01-15 13:39:37 +0000

    Jogo de Cartasdesupertrunfo desafio

---
+++ Cartasdesupertrunfo_aluno.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (){
+    printf("Desafio Super Trunfo - Países - Cadastro das Cartas\n");
+    int população = 320600;
+    float area = 1235.20;
+    float PIB = 359.64;
+    int turísticos = 58;
+    char cidade[20] = "Rio de janeiro";
+    char codigo[10] = "A09";
+
+    printf("população: %d habitantes\n", população);
+    printf("area: %.2f metros\n", area);
+    printf("PIB: %f reais\n", PIB);
+    printf("turisticos: %i pessoas\n", turísticos);
+    printf("cidade: %s\n", cidade);
+    printf("codigo: %s\n", codigo);
+
+    return 0;
+
+
+
+
+
+
+}
